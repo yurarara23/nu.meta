b4dbef08a757192aa474734d8942c4d2b3053f34
yurarara23 <urauraura2323@gmail.com>
2025-05-13 22:39:34 +0900

    仮削除、固定解除

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full z-50 flex justify-between items-center px-6 py-4 bg-black shadow-md text-white">
+    <header className="flex justify-between items-center px-6 py-4 bg-black shadow-md text-white">
       {/* 左：ロゴ＋タイトルをクリックでトップに戻る */}
       <Link href="/" className="flex items-center gap-2 hover:opacity-80">
         <Image src="/logo.png" alt="Logo" width={32} height={32} />
-        <span className="font-bold text-lg">NU.MetaCreate仮</span>
+        <span className="font-bold text-lg">Nu.MetaCreate</span>
       </Link>
     </header>
   );
