b336fbbe691de2086906748a1e8b9519f1e376cb
yurarara <urauraura2323@gmail.com>
2025-12-24 01:20:47 +0900

    自己紹介追加

---
+++ app/blog/yossi.md
@@ -0,0 +1,54 @@
+---
+title: "自己紹介8"
+image: "/yossi/2.png"
+author: "yossi"
+date: "2025-12-24"
+---
+
+## あいさつ
+
+はじめまして、yoshi_pawn です。\
+私は普段、理系の学生をしています。\
+Nu.メタ創の新規メンバーでわからないことも多いですが、がんばります！
+
+## 興味のあるツール
+
+### Blender
+
+昔からものづくりが好きで、高校の時に部活の先輩に教えてもらったのがきっかけです。
+今では VRChat で使用するためのアバター制作を勉強中です。
+
+<figure>
+  <img src="/yossi/1.png" alt="画像1" style="width:70%">
+</figure>
+
+<figure>
+  <img src="/yossi/2.png" alt="画像1" style="width:70%">
+</figure>
+
+<figure>
+  <img src="/yossi/3.png" alt="画像1" style="width:70%">
+</figure>
+
+<figure>
+  <img src="/yossi/4.png" alt="画像1" style="width:70%">
+</figure>
+
+### Unity
+
+Blender つながりで、作った 3D モデルを用いたゲームを制作したいと思ったことがきっかけです。
+勉強のために 2D でシューティングゲームやコマンドゲームを作っています。
+
+<figure>
+  <img src="/yossi/5.png" alt="画像1" style="width:70%">
+</figure>
+
+<figure>
+  <img src="/yossi/6.png" alt="画像1" style="width:70%">
+</figure>
+
+### 目標
+
+- ゲームキャラクターを Blender で制作する。
+- 3D ゲームを Unity で制作する。
+- ゲームを操作するためのコントローラーを電子工作する。
--- app/member/page.tsx
+++ app/member/page.tsx
@@ -13,51 +13,52 @@ const members = [
     name: "yurarara",
     year: "B2",
     image: "/icons/yurarara.png",
-    role: "プログラマー",
+    role: "エンジニア",
     slug: "yurarara",
   },
   {
     name: "水瓢瓜",
     year: "B2",
     image: "/icons/mizmelon.png",
-    role: "映像クリエイター",
+    role: "映像",
     slug: "mizmelon",
   },
   {
     name: "ぽてぃちゃん",
     year: "B2",
     image: "/icons/pothi.png",
-    role: "シナリオライター",
+    role: "シナリオ",
     slug: "pothi",
   },
   {
     name: "紗痲",
     year: "B2",
     image: "/syama/0.png",
-    role: "アシスタントディレクター",
+    role: "アシスタント",
     slug: "紗痲",
   },
   {
     name: "ルイ",
     year: "B2",
     image: "/icons/rui.png",
-    role: "3Dモデリング",
+    role: "モデリング",
     slug: "ルイ",
   },
   {
     name: "ALMA",
     year: "B3",
     image: "/icons/alma.jpg",
-    role: "サウンドクリエイター",
+    role: "サウンド",
     slug: "alma",
-  }
+  },
 ];
-
 
 export default function MemberListPage() {
   return (
     <div className="min-h-screen bg-black px-6 py-10">
-      <h1 className="text-3xl font-bold mb-8 text-center text-white">メンバー一覧</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center text-white">
+        メンバー一覧
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {members.map((member) => (
           <a
@@ -67,20 +68,20 @@ export default function MemberListPage() {
           >
             <div className="flex items-center space-x-4">
               {member.image ? (
-              <Image
-                    src={member.image}
-                    alt={member.name}
-                    width={64}
-                    height={64}
-                    className="rounded-full object-cover"
-                    />
-                ) : null}
+                <Image
+                  src={member.image}
+                  alt={member.name}
+                  width={64}
+                  height={64}
+                  className="rounded-full object-cover"
+                />
+              ) : null}
 
               <div>
-                <h2 className="text-xl font-semibold text-[#60a5fa]">{member.name}</h2>
-                <p className="text-sm text-gray-300">
-                  {member.year}
-                </p>
+                <h2 className="text-xl font-semibold text-[#60a5fa]">
+                  {member.name}
+                </h2>
+                <p className="text-sm text-gray-300">{member.year}</p>
                 <p className="text-sm text-gray-300">{member.role}</p>
               </div>
             </div>
